33c89682dc07ed8999bb0117e05f7ed03eb1f923
Igor Zubkov <icesik@altlinux.org>
2013-02-27 20:01:14 +0200

    Regenerate spree extension with spree 1.3

--- lib/spree_fulfillment.rb
+++ lib/spree_fulfillment.rb
@@ -1,19 +1,2 @@
 require 'spree_core'
-
-
-module SpreeFulfillment
-  class Engine < Rails::Engine
-
-    config.autoload_paths += %W(#{config.root}/lib)
-
-    def self.activate
-      Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
-        # For gem extensions, it appears that require is usable in dev and prod modes
-        # to load the patches once and only once.
-        require(c)
-      end
-    end
-
-    config.to_prepare &method(:activate).to_proc
-  end
-end
+require 'spree_fulfillment/engine'
