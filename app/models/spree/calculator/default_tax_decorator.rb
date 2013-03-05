f6a90337956045bf93cfb95d6f730d5e855968db
Igor Zubkov <icesik@altlinux.org>
2013-03-05 04:04:05 +0200

    Update for spree 1.3

--- app/models/spree/calculator/default_tax_decorator.rb
+++ app/models/spree/calculator/default_tax_decorator.rb
@@ -1,6 +1,6 @@
 # Fix a bug with rounding to the penny for sales tax.
 # See http://groups.google.com/group/spree-user/browse_thread/thread/374410d4dea30b6c
-Calculator::SalesTax.class_eval do
+Spree::Calculator::DefaultTax.class_eval do
   alias_method :orig_compute, :compute
 
   def compute(order)
