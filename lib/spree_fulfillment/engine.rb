9c2196efe13541bea13d4dd68412b0dd5c2ad890
Rémy Coutable <remy@rymai.me>
2017-04-27 01:16:55 +0200

    Remove obsolete files
    Signed-off-by: Rémy Coutable <remy@rymai.me>

--- lib/spree_fulfillment/engine.rb
+++ lib/spree_fulfillment/engine.rb
@@ -4,8 +4,6 @@ module SpreeFulfillment
     isolate_namespace Spree
     engine_name 'spree_fulfillment'
 
-    config.autoload_paths += %W(#{config.root}/lib)
-
     # use rspec for tests
     config.generators do |g|
       g.test_framework :rspec
